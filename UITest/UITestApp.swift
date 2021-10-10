7e372791894fd3033c5fd92135bec03d3be0eecc
Ali Karababa <karababa.alii@gmail.com>
2021-10-10 21:12:29 +0300

    Initial Commit

---
+++ UITest/UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestApp.swift
+//  UITest
+//
+//  Created by Ali Karababa on 10.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
