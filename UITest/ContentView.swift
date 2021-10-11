8dc9650a94d6cb225e79fca8f0961a6b80fb80c5
Ali Karababa <karababa.alii@gmail.com>
2021-10-11 19:23:37 +0300

    In order to learn new UI framework, I started with CodeWithChris!, In tutorial, we have learned a lots of things with practice, According to this work, I tried to create card game in order to experience about SwiftUI

--- UITest/ContentView.swift
+++ UITest/ContentView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerCard = "card2"
+    @State private var cpuCard = "card2"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack() {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    // MARK: - Update the cards
+                    self.playerCard = "card\(playerRand)"
+                    self.cpuCard = "card\(cpuRand)"
+                    // MARK: Update the scores
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }else if playerRand == cpuRand{
+                        return
+                    }else {
+                        cpuScore += 1
+                    }
+                }) {
+                    Image("dealbutton")
+                }
+                Spacer()
+                HStack() {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
